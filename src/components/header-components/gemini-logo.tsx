4174fecefde94020b5e044e409c23749332faa09
AetherMind-Ai <clashinkofsr@gmail.com>
2024-11-19 18:13:56 +0200

    Update gemini-logo.tsx

--- src/components/header-components/gemini-logo.tsx
+++ src/components/header-components/gemini-logo.tsx
@@ -1,6 +1,4 @@
-
 import { FaCaretDown, FaRegCheckCircle } from "react-icons/fa";
-import { SiGooglegemini } from "react-icons/si";
 import DevButton from "../dev-components/dev-button";
 import DevPopover from "../dev-components/dev-popover";
 
@@ -9,7 +7,7 @@ const GeminiLogo = () => {
       <DevPopover
         popButton={
           <DevButton size="sm" rounded="sm" className="text-lg gap-2">
-            Gemini
+            MindBot
             <FaCaretDown />
           </DevButton>
         }
@@ -22,28 +20,13 @@ const GeminiLogo = () => {
           >
             <span className="flex items-center gap-2">
               <SiGooglegemini className="text-lg text-[#4E82EE]" />
-              Gemini
+              MindBot
             </span>
             <FaRegCheckCircle className="text-xl" />
-          </DevButton>
-          <DevButton
-            ripple={false}
-            className="cursor-auto w-full !justify-start gap-3 group"
-            rounded="none"
-          >
-            <span className="flex items-center gap-2 opacity-50">
-              <SiGooglegemini className="text-lg text-[#D96570]" />
-              Gemini Advanced
-            </span>
-  
-            <DevButton variant="v1" rounded="sm">
-              Upgrade
-            </DevButton>
           </DevButton>
         </div>
       </DevPopover>
     );
   };
 
-  export default GeminiLogo
-  
+export default GeminiLogo;
